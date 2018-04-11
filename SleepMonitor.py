5c50a95b96001a4870f0b08d12c024d2bf7c6cc8
davepcollins83 <davidcollins.409@gmail.com>
2018-04-11 19:19:13 +0100

    Lamp migrated to direct RPi control

--- SleepMonitor.py
+++ SleepMonitor.py
@@ -18,11 +18,17 @@ import io
 import glob
 import time
 
-# added for web templating
-# import jinja2
+from neopixel import *
 
-# added to play sound
-# import pyglet
+# LED strip configuration:
+LED_COUNT      = 7      # Number of LED pixels.
+LED_PIN        = 10      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0
+LED_STRIP      = ws.SK6812_STRIP_GRBW
 
 # added for i2c
 import smbus
@@ -514,13 +520,19 @@ class ToggleLamp(resource.Resource):
 		white = getattr(self.app.config, 'white')
 
 		if self.lamp == 0:
-			writeDigispark(2, [1, red, green, blue, white])
+			#writeDigispark(2, [1, red, green, blue, white])
+			for i in range (0, 6):
+				self.app.strip.setPixelColor(i, Color(red, green, blue, white))
+				self.app.strip.show()
 			self.app.lamp = 1;
 			log(self.app.lamp)
 			onStr = 'on'
 			return onStr
 		else:
-			writeDigispark(2, [1, 0, 0, 0, 0])
+			#writeDigispark(2, [1, 0, 0, 0, 0])
+			for i in range (0, 6):
+				self.app.strip.setPixelColor(i, Color(0, 0, 0, 0))
+				self.app.strip.show()
 			self.app.lamp = 0;
 			log(self.app.lamp)
 			return 'off'
@@ -586,6 +598,11 @@ class SleepMonitorApp:
 
         spawnNonDaemonProcess(reactor, LoggingProtocol('sheep'), '/bin/sh',
                               ['sh', 'sheep_init.sh'])
+
+        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+    	# Intialize the library (must be called once before other functions).
+    	self.strip.begin()
+        self.strip.show()
 
         self.config = Config()
         self.reactor = reactor
